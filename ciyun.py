8ab7fb440d7fc60a8a7a38c0d8469011c099de98
Tsing Dai <daiqing2009@gmail.com>
2022-10-05 20:23:24 +0800

    add stopwords and change to heart shape

--- ciyun.py
+++ ciyun.py
@@ -1,15 +1,23 @@
 import matplotlib.pyplot as plt #数据可视化
 import jieba #词语切割
 import wordcloud #分词
-from wordcloud import WordCloud,ImageColorGenerator,STOPWORDS #词云，颜色生成器，停止词
+from wordcloud import WordCloud,ImageColorGenerator #词云，颜色生成器，停止词
 import numpy as np #科学计算
 from PIL import Image #处理图片
+import os
 
 def read_stopword(fpath):
     # 读取中文停用词表
     with open(fpath, 'r', encoding='utf-8') as file:
         stopword = file.readlines()
     return [word.replace('\n', '') for word in stopword]
+path='./stopwords'
+name_list = ['baidu_stopwords.txt', 'cn_stopwords.txt', 'hit_stopwords.txt', 'scu_stopwords.txt']
+
+stop_word = ["上帝","耶稣基督","耶稣","基督","事"]
+for fname in name_list:
+    stop_word += read_stopword(os.path.join(path, fname))
+stop_word = set(stop_word)
 
 def ciyun(): 
     # print(STOPWORDS)
@@ -18,14 +26,14 @@ def ciyun():
     wordlist = jieba.lcut(textfile)#切割词语
     space_list = ' '.join(wordlist) #空格链接词语
     # print(space_list)
-    backgroud = np.array(Image.open('data/world-map.jpg')) 
+    backgroud = np.array(Image.open('data\heart.png')) 
 	
     wc = WordCloud(width=1400, height=2200,
 			background_color='white',
 	        mode='RGB', 
 			mask=backgroud, #添加蒙版，生成指定形状的词云，并且词云图的颜色可从蒙版里提取
 			max_words=500,
-			stopwords=STOPWORDS.add('老年人'),#内置的屏蔽词,并添加自己设置的词语
+			stopwords=stop_word,#内置的屏蔽词,并添加自己设置的词语
 			font_path='C:\Windows\Fonts\STZHONGS.ttf',
 			max_font_size=150,
 			relative_scaling=0.6, #设置字体大小与词频的关联程度为0.4
@@ -39,7 +47,7 @@ def ciyun():
     plt.imshow(wc) #显示词云
     plt.axis('off') #关闭x,y轴
     plt.show()#显示
-    wc.to_file('test1_ciyun.jpg') #保存词云图
+    wc.to_file('target/test1_ciyun.jpg') #保存词云图
 
 def main():
     ciyun()
